63bcac076e44ef1d028c77426b53bcfea452a2c9
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-04-26 21:26:20 +0000

    Thu, Apr 27, 2023, 12:26 AM +03:00

--- llamas.c
+++ llamas.c
@@ -9,14 +9,21 @@ int main (void)
         start = get_int ("Starting size: ");
 
     }
-    while (start <9);
+    while (start < 9);
 
     int end;
     do
     {
         end = get_int ("Ending size: ");
     }
+    while (end < 0);
 
     int years = 0;
-    
+    while (start ==! end)
+    {
+        start += start / 12;
+        years++;
+    }
+
+    printf ("Years: %i\n", years);
 }
