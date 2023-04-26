8a337d9390ff4d85618c27d9562ca5e2371a3297
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-04-26 21:15:31 +0000

    Thu, Apr 27, 2023, 12:15 AM +03:00

--- phonebook.c
+++ phonebook.c
@@ -5,7 +5,7 @@
 int main (void)
 {
     string name = get_string("What is your name? ");
-    int age = get_int("\nWhat is your age?");
-    string number = get_string("\n What is your phone number? ");
+    int age = get_int("What is your age?");
+    string number = get_string("What is your phone number? ");
     printf("Name is %s. Age is %i. Number is %s.\n", name, age, number);
 }
