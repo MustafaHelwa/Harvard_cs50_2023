86d6ee2b630ad66388e2b9aa9d9a4764fe7257ba
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-04-28 17:17:51 +0000

    Fri, Apr 28, 2023, 8:17 PM +03:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,7 +3,14 @@
 
 int main(void)
 {
-    int height = get_int("Height: ");
+    int height;
+
+    //getting hight value between 1 and 8 only
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
 
     for(int i=0; i < height; i++)
     {
