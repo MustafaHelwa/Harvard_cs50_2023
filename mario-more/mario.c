497be249b204b4be5844eae322c3c651809e4573
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-04-28 18:21:17 +0000

    Fri, Apr 28, 2023, 9:21 PM +03:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -10,37 +10,41 @@ int main(void)
     {
         height = get_int("Height: ");
     }
-    while(height < 1 || height > 8);
+    while (height < 1 || height > 8);
 
 
-    //making first row spaces:
+    int row = 1;    //starting with row 1
+    int counter;    //counter for using it later for loops
 
-
-    int row = 1;
-    int counter;
+    //starting the whole loop for height
     do
-        {
+    {
+        //spacing based on the height and row
         for (counter = height - row; counter > 0; counter--)
         {
             printf(" ");
         }
 
+        //hashing based on the height and row
         for (counter = row; counter > 0; counter--)
         {
             printf("#");
         }
 
-        printf("  ");
+        printf("  ");   //duble spacing the triangle
 
+        //hashing based on the height and row - repeated
         for (counter = row; counter > 0; counter--)
         {
             printf("#");
         }
 
+        //moving to the next row
         printf("\n");
 
+        //adding value to row counter
         row++;
     }
-    while(row < height+1);
+    while (row < height + 1);
 
 }
