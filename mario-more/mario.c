ce2c6047a3bc95837afd078c83b8d29e0147c522
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-04-28 17:19:33 +0000

    Fri, Apr 28, 2023, 8:19 PM +03:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -5,13 +5,15 @@ int main(void)
 {
     int height;
 
-    //getting hight value between 1 and 8 only
+    //Getting hight value between 1 and 8 only
     do
     {
         height = get_int("Height: ");
     }
-    while (height < 1 || height > 8);
+    while(height < 1 || height > 8);
 
+
+    //Writing hash loop
     for(int i=0; i < height; i++)
     {
         printf("#  #\n");
