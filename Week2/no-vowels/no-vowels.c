bca6160590a9ae9cbd7c76c22ff8e4cc81be5136
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-04-29 17:02:02 +0000

    Sat, Apr 29, 2023, 8:02 PM +03:00

--- Week2/no-vowels/no-vowels.c
+++ Week2/no-vowels/no-vowels.c
@@ -9,44 +9,6 @@
 
 int main(int argc, string argv[])
 {
-    string leet[argc];
 
-    for (int i = 0; i < argc; i++)
-    {
-        leet[0][i] = argv[2][i];
-
-        char x = (char) leet[2][i];
-        switch (x)
-        {
-            case 'a':
-                printf("4");
-                break;
-
-            case 'e':
-                printf("3");
-                break;
-
-            case 'o':
-                printf("0");
-                break;
-
-            case 'u':
-                printf("v");
-                break;
-
-            case 'i':
-                printf("!");
-                break;
-
-            default:
-                printf("%s \n", leet[i]);
-                break;
-        }
-
-    }
-
-    for (int i = 0; i < argc; i++)
-    {
-        printf(" %s \n", leet[i]);
-    }
 }
+
