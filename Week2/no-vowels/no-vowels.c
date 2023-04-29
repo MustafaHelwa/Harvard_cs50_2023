7b4b29ae9b323c669c848ac59830843b58f7c9a6
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-04-29 17:16:55 +0000

    Sat, Apr 29, 2023, 8:16 PM +03:00

--- Week2/no-vowels/no-vowels.c
+++ Week2/no-vowels/no-vowels.c
@@ -6,9 +6,49 @@
 #include <cs50.h>
 #include <stdio.h>
 
+char replace (char vowel);
 
 int main(int argc, string argv[])
 {
+    char new_vowel;
 
+    for (int i = 0; i <= argc ; i++)
+    {
+        new_vowel = replace ((char) argv[1][i]);
+    }
+
+    printf("\n");
 }
 
+
+char replace (char vowel)
+{
+    switch (vowel)
+    {
+        case 'a':
+            printf("6");
+            vowel = '6';
+            break;
+
+        case 'e':
+            printf("3");
+            vowel = '3';
+            break;
+
+        case 'i':
+            printf("1");
+            vowel = '1';
+            break;
+
+        case 'o':
+            printf("0");
+            vowel = '0';
+            break;
+
+        default:
+            printf("%c", vowel);
+            break;
+    }
+    
+    return vowel;
+}
