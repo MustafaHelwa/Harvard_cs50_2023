23be7a6d9ab37c1ec0eccdbc693e6670a846077c
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-04-29 22:53:35 +0000

    Sun, Apr 30, 2023, 1:53 AM +03:00

--- Week2/substitution/substitution.c
+++ Week2/substitution/substitution.c
@@ -13,7 +13,7 @@ string key;                                     //global variable since it will 
 //##########################################################################//
 int main (int argc, string argv[])
 {
-    if (argc != 2)
+    if (argc != 1)
     {
         printf("Usage: ./substitution key");
     }
