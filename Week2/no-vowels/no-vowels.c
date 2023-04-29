22721a118a94512dd5478c686fb26684d769c76d
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-04-29 16:35:20 +0000

    Sat, Apr 29, 2023, 7:35 PM +03:00

--- Week2/no-vowels/no-vowels.c
+++ Week2/no-vowels/no-vowels.c
@@ -6,38 +6,38 @@
 #include <cs50.h>
 #include <stdio.h>
 
+char replacew (char x);
+
+
 int main(int argc, string argv[])
 {
     string leet[argc];
 
     for (int i = 0; i < argc; i++)
     {
-        leet[2][i] = argv[2][i];
-    }
-
-
-    for (int i = 0; i < argc; i++)
-    {
-        switch( leet[2][i])
-        {
-            case 'a':
-                printf("4");
-                break;
-
-            case 'b':
-                printf("8");
-                break;
-
-            default:
-                printf("\ndef\n");
-                break;
-
-        }
-        printf(" %s \n", argv[i]);
+    leet[i] = replacew ((char) argv[i]);
     }
 
     for (int i = 0; i < argc; i++)
     {
         printf(" %s \n", leet[i]);
     }
+}
+
+
+char replacew (char x[])
+{
+    switch (x)
+    {
+        case 'a':
+            printf("4");
+            break;
+        case 'b':
+            printf("8");
+            break;
+        default:
+            print("%s", x);
+            break;
+    }
+    return x;
 }
