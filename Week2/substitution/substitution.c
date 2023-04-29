0901fca35c4e6fe3518e958160d96a56160527c7
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-04-29 19:32:47 +0000

    Sat, Apr 29, 2023, 10:32 PM +03:00

--- Week2/substitution/substitution.c
+++ Week2/substitution/substitution.c
@@ -1,8 +1,22 @@
 #include <cs50.h>
+#include <string.h>
 #include <stdio.h>
 
+string keyChecker (string xVal);
 
-int main(int argc, string argv[])
+int main (int argc, string argv[])
 {
-    
+    key = keyChecker (argv[]);
+}
+
+string keyChecker (string xVal)
+{
+    if (strlen(sxVal) < 26)
+    {
+        printf("Key must contain 26 characters.");
+        return 1;
+    }
+
+
+    for (i < argc, )
 }
