9a636ad9dbf4ab2ea524cb3881ea81b1bf0540cb
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-04-29 21:55:27 +0000

    Sun, Apr 30, 2023, 12:55 AM +03:00

--- Week2/substitution/substitution.c
+++ Week2/substitution/substitution.c
@@ -6,8 +6,11 @@
 bool keyChecker1 (string keyvalue, int keylength);
 string processedtext (string tobedonetext);
 
-string key;
+string key;                                     //global variable since it will be used in all below functions
 
+//##########################################################################//
+//##################         Main code functions          ##################//
+//##########################################################################//
 int main (int argc, string argv[])
 {
     key = argv[1];
@@ -27,7 +30,10 @@ int main (int argc, string argv[])
     }
 }
 
-//26 letter checker
+
+//##########################################################################//
+//##################        key checking function         ##################//
+//##########################################################################//
 
 bool keyChecker1 (string keyvalue, int keylength)
 {
@@ -62,31 +68,36 @@ bool keyChecker1 (string keyvalue, int keylength)
     return 0;
 }
 
-
+//##########################################################################//
+//##################         processing function          ##################//
+//##########################################################################//
 string processedtext (string tobedonetext)
 {
+//getting the length of plaintext that will be processed
 
-    for (int i = 0; i < strlen(tobedonetext); i++)
+    int length = strlen(tobedonetext);
+
+//starting for loop to check and adjust the code value
+    for (int i = 0; i < length; i++)
     {
-        int tester = (int) tobedonetext[i];
 
-        if (islower(tester) != 0)
+        int tester = (int) tobedonetext[i];                 //integer to check the character of the array value
+
+        if (islower(tester) != 0)                           //small will be - 97 since a is 97 and it will be 0 in key array
         {
             int place = (int) tobedonetext[i] - 97;
-            tobedonetext[i] =  (char) islower((int) key [place]);
-            printf("failed1\n");
+            tobedonetext[i] =  tolower(key [place]);        //tolower() will make sure that it is lower
         }
-        else if (isupper(tester) != 0)
+        else if (isupper(tester) != 0)                      //repeated with upper case instead of lower
         {
             int place = (int) tobedonetext[i] - 65;
-            tobedonetext[i] =  (char) isupper((int) key [place]);
-            printf("failed2\n");
+            tobedonetext[i] =  toupper(key [place]);
         }
-        else
+        else if (isalpha(tester) == 0)                      //if it is neither upper nor lower it shouldn't be an alphabit char
         {
-            tobedonetext[i] = (char) tobedonetext[i];
-            printf("failed3\n");
+            tobedonetext[i] = tobedonetext[i];              //getting the same char value
         }
+
     }
-    return tobedonetext;
+    return tobedonetext;                                    //return encrypted line
 }
