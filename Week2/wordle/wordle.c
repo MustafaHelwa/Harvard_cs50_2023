e321f55d49ec30721e48b3d74f934bc635fb85d3
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-04-30 17:02:27 +0000

    Sun, Apr 30, 2023, 8:02 PM +03:00

--- Week2/wordle/wordle.c
+++ Week2/wordle/wordle.c
@@ -198,15 +198,15 @@ void print_word(string guess, int wordsize, int status[])
     {
         if (status[i] == 2)
         {
-            printf(GREEN"%c"RESET"\n", status[i]);
+            printf(GREEN"%c"RESET"\n", guess[i]);
         }
         else if (status[i] == 1)
         {
-            printf(YELLOW"%c"RESET"\n", status[i]);
+            printf(YELLOW"%c"RESET"\n", guess[i]);
         }
         else
         {
-            printf(RED"%c"RESET"\n", status[i]);
+            printf(RED"%c"RESET"\n", guess[i]);
         }
     }
 
