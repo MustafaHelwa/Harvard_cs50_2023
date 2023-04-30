0882efdae9d6356256ebb58b9220ca10223588ad
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-04-30 10:16:43 +0000

    Sun, Apr 30, 2023, 1:16 PM +03:00

--- Week2/wordle/wordle.c
+++ Week2/wordle/wordle.c
@@ -121,12 +121,13 @@ int main(int argc, string argv[])
 
 string get_guess(int wordsize)
 {
-    string guess = "";
+    string guess = get_string"";
 
     // ensure users actually provide a guess that is the correct length
     // TODO #3
-
     
+
+
     return guess;
 }
 
