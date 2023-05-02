99f981b0e886b3f59ed3594523bcfcbf3d59f2da
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-05-02 18:12:09 +0000

    Tue, May 2, 2023, 9:12 PM +03:00

--- Week3/plurality/plurality.c
+++ Week3/plurality/plurality.c
@@ -103,20 +103,20 @@ void print_winner(void)
         }
     }
 
-    if (candidates[0].votes == candidates[1].votes)
+    if (candidates[candidate_count].votes == candidates[candidate_count - 1].votes)
     {
-        if (candidates[1].votes == candidates[2].votes)
+        if (candidates[candidate_count - 1].votes == candidates[candidate_count - 2].votes)
         {
-            printf("%s \n%s \n%s", candidates[0].name, candidates[1].name,  candidates[2].name);
+            printf("%s \n%s \n%s", candidates[candidate_count].name, candidates[candidate_count - 1].name,  candidates[candidate_count - 2].name);
         }
         else
         {
-            printf("%s\n %s", candidates[0].name, candidates[1].name);
+            printf("%s\n %s", candidates[candidate_count].name, candidates[candidate_count - 1].name);
         }
     }
     else
     {
-        printf("%s\n", candidates[0].name);
+        printf("%s\n", candidates[candidate_count].name);
     }
 
 
