29002ebe907a335e4789ec169dc17e6e24cf1625
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-05-02 20:02:30 +0000

    Tue, May 2, 2023, 11:02 PM +03:00

--- Week3/tideman/tideman.c
+++ Week3/tideman/tideman.c
@@ -117,14 +117,14 @@ bool vote(int rank, string name, int ranks[])
 void record_preferences(int ranks[])
 {
     // TODO
-
+    //loop over the whole preferences combinations, if the rank is higher, record a preference + 1
     for (int i = 0; i < candidate_count; i++)
     {
         for (int j = 0; j < candidate_count; j++)
         {
             if(ranks[i] < ranks[j])
             {
-                
+                preferences[i][j] += 1;
             }
         }
     }
