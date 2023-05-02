a9818d3463420757fed6feed4de70f79870f79df
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-05-02 20:29:42 +0000

    Tue, May 2, 2023, 11:29 PM +03:00

--- Week3/tideman/tideman.c
+++ Week3/tideman/tideman.c
@@ -121,24 +121,42 @@ void record_preferences(int ranks[])
 {
     // TODO
     //loop over the whole preferences combinations, if the rank is higher, record a preference + 1
+    // Assess voter ranks
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
+        for (int j = i + 1; j < candidate_count; j++)
         {
-            if(ranks[i] < ranks[j])
-            {
-                preferences[i][j] += 1;
-            }
+            // Update number of voters who prefer [i] to [j] in ranks array
+            preferences[ranks[i]][ranks[j]]++;
         }
     }
-    return;
+return;
 }
 
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
     // TODO
-
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            if (preferences[i][j] > preferences[j][i])
+            {
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
+                pair_count++;
+            }
+            else if (preferences[i][j] < preferences[j][i])
+            {
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
+                pair_count++;
+            }
+        }
+    }
+    return;
+}
 
     return;
 }
@@ -147,6 +165,20 @@ void add_pairs(void)
 void sort_pairs(void)
 {
     // TODO
+    for (int i = pair_count - 1; i >= 0 ; i--)
+    {
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if ((preferences[pairs[j].winner][pairs[j].loser]) < (preferences[pairs[j + 1].winner][pairs[j + 1].loser]))
+            {
+                pair temp = pairs[j];
+                pairs[j] = pairs[j + 1];
+                pairs[j + 1] = temp;
+            }
+        }
+    }
+return;
+}
     return;
 }
 
