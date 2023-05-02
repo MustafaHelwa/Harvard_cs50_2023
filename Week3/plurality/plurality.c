98d9341b527d230a2b47091690719b9f81196a54
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-05-02 18:23:04 +0000

    Tue, May 2, 2023, 9:23 PM +03:00

--- Week3/plurality/plurality.c
+++ Week3/plurality/plurality.c
@@ -105,15 +105,12 @@ void print_winner(void)
 
     printf("%s\n", candidates[candidate_count].name);
 
-    if (candidates[candidate_count].votes == candidates[candidate_count - 1].votes)
+    for (int i = 1; i < candidate_count; i++)
     {
-       for (int i = 0; i < candidate_count; i++)
-       {
         if (candidates[candidate_count].votes == candidates[candidate_count - i].votes)
         {
             printf("%s\n", candidates[candidate_count - i].name);
         }
-       }
     }
 
 
