e5aad9819704fce6bae301988f0711a49f2e57b1
Mustafa Helwa <65976495+MustafaHelwa@users.noreply.github.com>
2023-09-13 12:06:32 +0000

    Wed, Sep 13, 2023, 3:06 PM +03:00

--- Week4/bottomup/bottomup.c
+++ Week4/bottomup/bottomup.c
@@ -56,8 +56,6 @@ int main(int argc, char *argv[])
     // Write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
-    Set biHeight to negative bi.biHeight = - (bi.biHeight);
-
     // Write outfile's BITMAPINFOHEADER
     fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
 
